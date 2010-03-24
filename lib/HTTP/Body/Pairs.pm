7d02eaa13ae5f95dcb8182c964184e125e97de12
Paul Driver <frodwith@cpan.org>
2010-03-24 23:16:20 +0000

    Import of FRODWITH/HTTP-Body-Pairs-0.2 from CPAN.
    gitpan-cpan-distribution: HTTP-Body-Pairs
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         FRODWITH/HTTP-Body-Pairs-0.2.tar.gz
    gitpan-cpan-author:       FRODWITH
    gitpan-cpan-maturity:     released

--- lib/HTTP/Body/Pairs.pm
+++ lib/HTTP/Body/Pairs.pm
@@ -1,5 +1,5 @@
-package HTTP::Body;
-our $VERSION = '0.1';
+package HTTP::Body::Pairs;
+our $VERSION = '0.2';
 
 # ABSTRACT: methods for getting body params in the order they were sent
 
@@ -19,9 +19,9 @@ use warnings;
     };
 }
 
-sub flat_pairs { @{ $_[0]->{pairs} } }
+sub HTTP::Body::flat_pairs { @{ $_[0]->{pairs} } }
 
-sub pairs {
+sub HTTP::Body::pairs {
     my $a = $_[0]->{pairs};
     my @result;
     for (my $i = 0; $i < @$a; $i += 2) {
@@ -38,7 +38,7 @@ HTTP::Body::Pairs
 
 =head1 VERSION
 
-version 0.1
+version 0.2
 
 =head1 SYNOPSIS
 
